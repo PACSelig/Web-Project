8aa1d55cacf73ee6ced0daaa2be1cd0e06d1766a
Chris <Chris@MacBook-Pro.hsd1.pa.comcast.net>
2019-02-09 02:25:00 -0500

    Init

---
+++ JS/titleScript.js
@@ -0,0 +1,40 @@
+var toggle = false;
+function openMenu() {
+    var pos = 13;
+    var elem = document.getElementById("sidemenu");   
+    var id = setInterval(frame, 3);
+    function frame() {
+        if (pos == 0) {
+            clearInterval(id);
+        } else {
+            if(pos > 0.1){
+                pos -= 0.1;
+                elem.style.right = pos + 'em';
+                }
+            }
+    }
+}
+function closeMenu() {
+    var pos = 0;
+    var elem = document.getElementById("sidemenu");   
+    var id = setInterval(frame, 3);
+    function frame() {
+        if (pos == 13) {
+            clearInterval(id);
+        } else {
+            if(pos < 13){
+                pos += 0.1;
+                elem.style.right = pos + 'em';
+            }
+        }
+    }
+}
+function menuToggle(){
+    if(toggle == false){
+        openMenu();
+        toggle = !toggle;
+    }else if(toggle == true) {
+        closeMenu();
+        toggle = !toggle;
+    }
+}
