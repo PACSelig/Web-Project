1bce54fc98636110a1ad0b258c41d83bbeecf481
Chris <cseligemail@gmail.com>
2019-02-20 17:26:14 -0500

    Commit

--- JS/titleScript.js
+++ JS/titleScript.js
@@ -1,14 +1,22 @@
 var toggle = false;
 var elem = document.getElementById("menuContainer");
+var body = document.getElementById("body");
+var content = document.getElementsByClassName("container")[0];
+var scrollPosY, scrollPosX;
 function openMenu() {
   var pos = -100;
   var id = setInterval(frame, 4);
+  scrollPosY = window.scrollY
+  scrollPosX = window.scrollX;
+  elem.style.height = "100vh";
+  body.style.overflow = "hidden";
+  window.scrollTo(0,0);
   function frame() {
     if (pos === 0) {
       clearInterval(id);
     } else {
       if (pos < 0) {
-        pos += 1;
+        pos += 1
         elem.style.left = pos + "%";
       }
     }
@@ -20,6 +28,9 @@ function closeMenu() {
   function frame() {
     if (pos === -100) {
       clearInterval(id);
+      elem.style.height = "calc(100% - 42.5px)";
+      body.style.overflow = "unset";
+      window.scrollTo(scrollPosX, scrollPosY);
     } else {
       if (pos > -100) {
         pos -= 1;
