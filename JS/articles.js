90545725eed3abf61862808a291ec99abb4b81b4
Chris <cseligemail@gmail.com>
2019-03-04 20:25:45 -0500

    Commit

--- JS/articles.js
+++ JS/articles.js
@@ -1,11 +1,7 @@
 let article = document.getElementById("main-container").getElementsByTagName("article")[0];
 
 function appendElem(title, content) {
-<<<<<<< HEAD
-let template = `
-=======
   let template = `
->>>>>>> refs/remotes/origin/master
   <div class="Article">
     <h1 id="main-Title">${title}</h1>
     <hr>
@@ -17,30 +13,6 @@ let template = `
 }
 
 function saveArticles() {
-<<<<<<< HEAD
-    const element = article.getElementsByClassName("Article");
-    let index = 1;
-    for(var art of element){
-      console.info(art);
-      localStorage.setItem('Article' + index, art.outerHTML);
-      index++;
-    }
-}
-
-function loadArticles() {
-  article.innerHTML = "";
-  for (let i = 1; i <= localStorage.length; i++) {
-    const element = localStorage.getItem("Article" + i);
-    article.innerHTML = element + article.innerHTML;
-
-  }
-}
-
-function removeArticle(keyNum){
-  article = article - localStorage.getItem("article"+keyNum);
-  saveArticles();
-}
-=======
   localStorage.clear();
   const element = article.getElementsByClassName("Article");
   let index = 1;
@@ -66,4 +38,3 @@ function removeArticle(keyNum) {
   article = article - localStorage.getItem("article" + keyNum);
   saveArticles();
 }
->>>>>>> refs/remotes/origin/master
